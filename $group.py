2b4c483bef2bb76a84d2f7c7ed5584b85d8ac190
Tejaswinimiriyala <158312630+Miriyala7@users.noreply.github.com>
2024-03-01 22:32:45 -0500

    Update $group.py

--- $group.py
+++ $group.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://tejum2108:mine2108@cluster0.1dc7j9s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.your_database_name
+db = client.sample_training
 zips_collection = db.zips
 # Aggregation pipeline
 pipeline = [
