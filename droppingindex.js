6525b51ecb90d66b107aacec776e1849ac51c55f
Tejaswinimiriyala <158312630+Miriyala7@users.noreply.github.com>
2024-03-13 16:08:31 -0400

    Create droppingindex.js

---
+++ droppingindex.js
@@ -0,0 +1,15 @@
+droping index
+
+db.student.dropIndex({key: {student_id:551}},
+{
+			"createdCollectionAutomatically": false,
+			"numIdexesBefore":1,
+			"numIndexesAfter":2,
+			"ok":1
+})
+output:{
+  ok: 0,
+  errmsg: "can't find index with key: { key: { student_id: 551 } }",
+  code: 27,
+  codeName: 'IndexNotFound'
+}
